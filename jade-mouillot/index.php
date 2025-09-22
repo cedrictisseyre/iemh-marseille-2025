8fe23f6e61dc71cd5438508adacaee2df76cfe7c
jadeiseng <153304167+jadeiseng@users.noreply.github.com>
2025-09-22 13:35:54 +0000

    premier commit

---
+++ jade-mouillot/index.php
@@ -0,0 +1,14 @@
+<?php
+$a = 10;
+$b = 5;
+
+$addition = $a + $b;
+$soustraction = $a - $b;
+$multiplication = $a * $b;
+$division = $a / $b;
+
+echo "Addition : $a + $b = $addition<br>";
+echo "Soustraction : $a - $b = $soustraction<br>";
+echo "Multiplication : $a * $b = $multiplication<br>";
+echo "Division : $a / $b = $division<br>";
+?>
