4c6afdc56c42d0dd8b5f2376cc4774f248234008
Chloedbrd <chloe.desbordes@etu.univ-amu.fr>
2025-09-22 13:57:31 +0000

    Ajout de la fonction valeurAbsolue avec un exemple d'utilisation

---
+++ chloe-desbordes/Valeur absolue.php
@@ -0,0 +1,9 @@
+<?php
+function valeurAbsolue($terme) {
+    return abs($terme);
+}
+
+// Exemple d'utilisation
+$nombre = -15;
+echo "La valeur absolue de $nombre est " . valeurAbsolue($nombre);
+?>
