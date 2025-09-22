eb87097ca33463270a81ce8fd1f4b4e7ebda1827
cedrictisseyre <c.tisseyre@gmail.com>
2025-09-22 09:17:32 +0000

    Index. PHP, pour tester l'environnement.

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>IEHM. Marseille, 2025</title>
+</head>
+<body>
+	<h1>IEHM. Marseille, 2025</h1>
+	<p>Bienvenue sur la page de test de l'environnement PHP pour le projet IEHM.</p>
+	<hr>
+	<h2>Informations PHP</h2>
+	<?php phpinfo(); ?>
+</body>
+</html>
