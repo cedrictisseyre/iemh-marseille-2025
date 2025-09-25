9480ba3763ce0f32807f2fa4f15e3e3272ce192c
copntl <colinepontal@hotmail.fr>
2025-09-25 12:34:25 +0000

    Test db connexion coline-pontal

--- coline-pontal/db_connexion.php
+++ coline-pontal/db_connexion.php
@@ -1,10 +1,8 @@
 <?php
-// Connexion à la base de données MySQL
-
 $host = '195.15.235.20';
 $user = 'root';
-$password = 'INNnsk40374';
-$dbname = 'coline_pontal'; // Ajoute le nom de la base
+$password = 'INNnsk40374'; // ou ton propre mot de passe
+$dbname = 'coline_pontal';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
@@ -12,6 +10,3 @@ try {
 } catch (PDOException $e) {
     die('Erreur de connexion à la base de données : ' . $e->getMessage());
 }
-
-// ... Utilisez $conn pour vos requêtes SQL ...
-?>
