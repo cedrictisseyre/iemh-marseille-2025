5bd34e1e9e0dd0c86f7f09915885b49c850c635f
andoguerin <135982036+andoguerin@users.noreply.github.com>
2025-09-25 14:09:29 +0000

    Activation de l'affichage des erreurs PHP dans index.php

--- ando-guerin/index.php
+++ ando-guerin/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'connexion.php';
 
 // Récupérer les jours et horaires pour l'emploi du temps
