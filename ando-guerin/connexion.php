991d1ff273321d1f0b58392f5efc20137ad56d72
andoguerin <135982036+andoguerin@users.noreply.github.com>
2025-09-25 13:53:32 +0000

    Correction connexion.php (affichage erreurs ou autres modifs)

--- ando-guerin/connexion.php
+++ ando-guerin/connexion.php
@@ -5,7 +5,7 @@ $password = 'INNnsk40374';
 $dbname = 'Andoni_guerin';
 
 try {
-    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname", $user, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Erreur de connexion à la base de données : ' . $e->getMessage());
