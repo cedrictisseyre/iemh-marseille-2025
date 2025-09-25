975829280e18efa756153537eb07b8da93bda0e5
jadeiseng <153304167+jadeiseng@users.noreply.github.com>
2025-09-25 12:38:39 +0000

    Ajout page d'accueil avec menu déroulant et pages d'ajout (sportif, club, course, discipline)

---
+++ jade-mouillot/services/afficher_course.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../connexion.php';
+$sql = "SELECT * FROM courses";
+$stmt = $conn->query($sql);
+$cours = $stmt->fetchAll(PDO::FETCH_ASSOC);
+header('Content-Type: application/json');
+echo json_encode($courses);
+?>
---
+++ jade-mouillot/services/afficher_discipline.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../connexion.php';
+$sql = "SELECT * FROM disciplines";
+$stmt = $conn->query($sql);
+$cours = $stmt->fetchAll(PDO::FETCH_ASSOC);
+header('Content-Type: application/json');
+echo json_encode($disciplines);
+?>
