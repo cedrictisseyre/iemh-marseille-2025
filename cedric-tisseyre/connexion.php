922c99d27a3616a15d64573b8b55df52ef9f957f
cedrictisseyre <c.tisseyre@gmail.com>
2025-09-25 11:33:43 +0000

    Suppression du charset pour la connexion PDO

--- cedric-tisseyre/connexion.php
+++ cedric-tisseyre/connexion.php
@@ -7,7 +7,7 @@ $password = 'INNnsk40374';
 $dbname = 'cedric_tisseyre'; // Ajoute le nom de la base
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Erreur de connexion à la base de données : ' . $e->getMessage());
