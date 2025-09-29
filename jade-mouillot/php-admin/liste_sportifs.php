da80d5ceb90365873d6b6d4a0f9caeb050eecc0a
jadeiseng <153304167+jadeiseng@users.noreply.github.com>
2025-09-29 17:24:12 +0000

    Modifications manuelles sur liste_sportifs.php

--- jade-mouillot/php-admin/liste_sportifs.php
+++ jade-mouillot/php-admin/liste_sportifs.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'db_connect.php';
 
 $sql = "SELECT s.id, s.nom, c.nom AS club, co.nom AS course, d.nom AS discipline
