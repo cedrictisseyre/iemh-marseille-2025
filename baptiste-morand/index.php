35ee8c048389dfa4a6dc34689a733951bef68389
baptistemorand2803-glitch <baptiste.morand2803@gmail.com>
2025-10-02 13:53:05 +0000

    Mise à jour de la page index.php avec menu de navigation vers les pages HTML

--- baptiste-morand/index.php
+++ baptiste-morand/index.php
@@ -1,5 +1,25 @@
-<?php
 // index.php - Page d'accueil pour Baptiste Morand
 
 echo "Bienvenue sur la page de Baptiste Morand !";
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Accueil Baptiste Morand</title>
+</head>
+<body>
+	<h1>Bienvenue sur la page de Baptiste Morand !</h1>
+	<h2>Navigation</h2>
+	<ul>
+		<li><a href="pages/liste_games.html">Liste des matchs</a></li>
+		<li><a href="pages/liste_players.html">Liste des joueurs</a></li>
+		<li><a href="pages/liste_teams.html">Liste des équipes</a></li>
+		<li><a href="pages/liste_stats.html">Liste des stats joueurs</a></li>
+		<li><a href="pages/ajout_game.html">Ajouter un match</a></li>
+		<li><a href="pages/ajout_player.html">Ajouter un joueur</a></li>
+		<li><a href="pages/ajout_team.html">Ajouter une équipe</a></li>
+		<li><a href="pages/ajout_stats.html">Ajouter des stats joueur</a></li>
+	</ul>
+</body>
+</html>
