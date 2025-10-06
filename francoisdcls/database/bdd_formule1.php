006db1630a6365fd22ed25026901b4106658bab5
francoisdcls <francois.duclos26@gmail.com>
2025-10-06 10:15:35 +0000

    Commenter la ligne d'affichage de la connexion réussie dans bdd_formule1.php

--- francoisdcls/database/bdd_formule1.php
+++ francoisdcls/database/bdd_formule1.php
@@ -12,7 +12,7 @@ try {
     // Connexion à la base de données
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie !<br>";
+    // echo "Connexion réussie !<br>";
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
