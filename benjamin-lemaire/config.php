811686e5f5c0c6217803ff2f25de54b6899d2a59
Benjamin Lemaire <lemaire.benjamin.05@gmail.com>
2025-10-06 08:50:25 +0000

    Correction charset utf8 pour connexion PDO

--- benjamin-lemaire/config.php
+++ benjamin-lemaire/config.php
@@ -5,7 +5,7 @@ $user = 'root'; // À adapter si besoin
 $pass = 'INNnsk40374';
 
 try {
-	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb3", $user, $pass);
+	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	die('Erreur de connexion à la base de données : ' . $e->getMessage());
