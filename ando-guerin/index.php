cffed32e721acdd0d6dbecf0409a3e6c4459541c
andoguerin <135982036+andoguerin@users.noreply.github.com>
2025-10-06 08:23:46 +0000

    Suppression de l'affichage des erreurs PHP dans index.php (site en production)

--- ando-guerin/index.php
+++ ando-guerin/index.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once 'connexion.php';
 
 // Récupérer les jours et horaires pour l'emploi du temps
