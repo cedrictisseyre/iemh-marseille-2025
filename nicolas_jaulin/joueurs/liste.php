5bfd85c8ee8be66965a0b94014173d0a8a68d3bb
nicolas-jaulin <nicolas.jaulin@sfr.fr>
2025-10-06 11:11:57 +0000

    Correction des chemins d'inclusion de db_connect.php dans les pages liste.php

--- nicolas_jaulin/joueurs/liste.php
+++ nicolas_jaulin/joueurs/liste.php
@@ -5,7 +5,7 @@ if ($php_debug) {
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
-require_once 'db_connect.php';
+require_once '../db_connect.php';
 $stmt = $pdo->query('SELECT * FROM Joueurs');
 ?>
 <!DOCTYPE html>
--- nicolas_jaulin/matchs/liste.php
+++ nicolas_jaulin/matchs/liste.php
@@ -5,7 +5,7 @@
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
     }
-require_once 'db_connect.php';
+require_once '../db_connect.php';
 $stmt = $pdo->query('SELECT * FROM Matchs');
 ?>
 <!DOCTYPE html>
