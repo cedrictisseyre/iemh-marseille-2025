9265e69031600891cc93cb2e3f3fc7e13a48cc93
thomasfajolle <thomasfajolle@gmail.com>
2025-10-06 16:14:51 +0000

    modification connection

--- thomas-fajolle/connexion.php
+++ thomas-fajolle/connexion.php
@@ -1,12 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'thomas_fajolle';
-$username = 'root';
-$password = ''; // ou ton mot de passe MySQL
+$host = '195.15.235.20';       // IP fournie par ton prof
+$dbname = 'thomas_fajolle';    // Nom de ta base
+$username = 'root';            // Login fourni
+$password = 'INNnsk40374';    // Mot de passe fourni
+$port = 3306;                  // Port MySQL par défaut
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connexion réussie"; // Décommenter juste pour tester
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
