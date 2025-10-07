253aeca37acc65a90677c3e6dd043a15d62c7bf5
BriacDeschaux <briac.deschaux@etu.univ-amu.fr>
2025-10-07 09:07:42 +0000

    corrrection bug

--- briac-deschaux/NFL_Stats_Analyzer.php
+++ briac-deschaux/NFL_Stats_Analyzer.php
@@ -1,7 +1,9 @@
-<?<?php
-declare(strict_types=1);
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-require_once __DIR__ . '/config/database_connexion.php';
+include __DIR__ . '/config/database_connexion.php';
 require_once __DIR__ . '/services/helpers.php';
 
 // page selection
