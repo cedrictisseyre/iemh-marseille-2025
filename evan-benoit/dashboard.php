f6d3a1c7e758902778f64d94579280e1eb642fce
evanbnt <evan.benoit44@icloud.com>
2025-10-08 19:39:23 +0000

    .

---
+++ evan-benoit/dashboard.php
@@ -0,0 +1,15 @@
+<?php include 'header.php'; ?>
+
+<div class="text-center mt-5">
+    <h1>🏋️ Bienvenue sur ton espace Coaching Sportif</h1>
+    <p class="lead mt-3">Gère facilement tes séances, tes clients et ton suivi d’entraînement.</p>
+
+    <div class="mt-4">
+        <a href="index.php" class="btn btn-primary mx-2">📅 Voir les séances</a>
+        <a href="clients.php" class="btn btn-success mx-2">👥 Voir les clients</a>
+        <a href="coachs.php" class="btn btn-warning mx-2">👨‍🏫 Voir les coachs</a>
+        <a href="masse.php" class="btn btn-info mx-2">📊 Suivi masse</a>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
