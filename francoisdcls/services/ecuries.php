82ca8c171cffdf0a32d8f28683d159577d3617bd
francoisdcls <francois.duclos26@gmail.com>
2025-10-14 08:57:03 +0000

    Fix: services/ecuries.php use nom_ecuries column

--- francoisdcls/services/ecuries.php
+++ francoisdcls/services/ecuries.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
 require_once __DIR__ . '/../database/bdd_formule1.php';
-$sql = "SELECT * FROM ecuries ORDER BY nom";
+$sql = "SELECT ecurie_id, nom_ecuries FROM ecuries ORDER BY nom_ecuries";
 $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($rows);
