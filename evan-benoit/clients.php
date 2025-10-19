3bbd1011bc114ecf8e708300615eec34a57832e3
evanbnt <evan.benoit44@icloud.com>
2025-10-19 10:41:14 +0000

    login

--- evan-benoit/clients.php
+++ evan-benoit/clients.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+<?php
 include 'header.html';
 require 'connexion.php';
 
--- evan-benoit/seances.php
+++ evan-benoit/seances.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+<?php
 require_once 'connexion.php';
 include 'header.html';
 
